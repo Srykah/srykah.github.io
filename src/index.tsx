9082e9a806130d40d94f0010ba30ff8ebd78b558
Thomas Bécavin <srykah@gmail.com>
2020-12-24 06:27:18 +0100

    [Feat] Dark mode

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "../node_modules/bulma/css/bulma.min.css";
 
+document.getElementsByTagName("html")[0].className = "has-background-dark";
+const root = document.getElementById("root");
+if (root?.className !== undefined) {
+	root.className = "has-text-light";
+}
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-	document.getElementById("root")
+	root
 );
